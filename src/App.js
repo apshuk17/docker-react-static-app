3dc2f947f1e2eba2bf9b5572e707df13fd0eb7fa
Apoorva Shukla <apoorva.shukla@publicissapient.com>
2020-08-03 12:02:21 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [count, setCount] = useState(1);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.log("##err", err));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Posts</h1>
+      <div className="container">
+        {posts
+          .filter((item, index) => index < count * 10)
+          .map(({ id, title }) => {
+            return <p key={id} className="title">{title}</p>;
+          })}
+      </div>
+      {count < 5 ? <button onClick={() => setCount(prevState => prevState + 1)}>Show more</button>: null}
     </div>
   );
 }
